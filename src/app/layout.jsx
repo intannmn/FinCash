f192e493410356079b6ab254de25ca4e098f93fd
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-19 05:33:34 +0700

    feat - Landing Page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "FinCash | Login",
+  title: "FinCash | Landing Page",
 };
 
 export default function RootLayout({ children }) {
