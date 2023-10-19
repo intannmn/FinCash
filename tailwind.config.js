0a891a6c45007311842ffecaf544f326a378cccb
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-19 07:15:10 +0700

    dependencies - Add Background | Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "dots-background": "url('/assets/background/dots-background.svg')",
+      },
       colors: {
         myPurple: "#6152ff",
         myPurpleSec: "#4e3eff",
         myDarkPurple: "#2f3349",
+        myGreenA: "#b6e6a2",
+        myGreenB: "#5eda2a",
+        myGreenC: "#dedf38",
+        myGreenD: "#1a6104",
       },
     },
   },
