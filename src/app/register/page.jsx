b20ef0146f005b9bdbed49bddb6847958f985d27
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-22 04:43:20 +0700

    update - Register

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,28 +1,41 @@
 "use client";
+
+// IMPORT LIBRARIES
 import Image from "next/image";
 import { useState } from "react";
+
+// IMPORT COMPONENTS
+import InputPassword from "@/components/inputs/InputPassword";
+import InputText from "@/components/inputs/InputText";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [invalid, setInvalid] = useState(false);
+  const [invalidLabel, setInvalidLabel] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const authentication = (e) => {
+  // HANDLE SUBMIT
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (confirmPassword === password) {
-      try {
-        console.log(username, password, confirmPassword);
-        setInvalid(false);
-      } catch (error) {
-        console.error("Error:", error);
-        throw error;
+    setLoading(true);
+    setTimeout(() => {
+      if (username && password && confirmPassword) {
+        if (password === confirmPassword) {
+          setLoading(false);
+          alert(`Username: ${username}\nPassword: ${password}`);
+        } else {
+          setLoading(false);
+          setInvalidLabel("Confirm Password Do Not Match");
+          setInvalid(true);
+        }
+      } else {
+        setLoading(false);
+        setInvalidLabel("Can Not Be Empty");
+        setInvalid(true);
       }
-    } else {
-      setPassword("");
-      setConfirmPassword("");
-      setInvalid(true);
-    }
+    }, 2000);
   };
 
   return (
@@ -30,77 +43,63 @@ export default function Register() {
       <div className="bg-dots-background bg-no-repeat bg-fixed bg-cover -z-10 fixed w-full h-full" />
       <div className="space-y-5 p-5 w-[400px] bg-myGreenA/50 rounded-lg border-2 border-black">
         {/* LOGO */}
-        <Image src={require("@/assets/logo/FinCash.svg")} alt="FinCash" className="mx-auto" />
-        {/* LOGO */}
+        <Image
+          src={require("@/assets/logo/FinCash.svg")}
+          alt="FinCash"
+          quality={50}
+          className="mx-auto"
+        />
 
-        <form onSubmit={authentication} className="space-y-5 w-full mx-auto">
+        <form onSubmit={handleSubmit} className="space-y-5 w-full mx-auto">
           {/* INPUT USERNAME */}
-          <fieldset className="border-2 group/username focus-within:border-myGreenD border-black rounded-md px-3 pb-2 w-full">
-            <legend className="group-focus-within/username:text-myGreenD px-2 font-semibold">
-              Username
-            </legend>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              className="bg-transparent outline-none w-full"
-            />
-          </fieldset>
-          {/* INPUT USERNAME */}
+          <InputText
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
           {/* INPUT PASSWORD */}
-          <fieldset className="border-2 group/password focus-within:border-myGreenD border-black rounded-md px-3 pb-2 w-full">
-            <legend className="group-focus-within/password:text-myGreenD px-2 font-semibold">
-              Password
-            </legend>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="bg-transparent outline-none w-full"
-            />
-          </fieldset>
-          {/* INPUT PASSWORD */}
+          <InputPassword
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
           {/* INPUT CONFIRM PASSWORD */}
-          <fieldset className="border-2 group/password focus-within:border-myGreenD border-black rounded-md px-3 pb-2 w-full">
-            <legend className="group-focus-within/password:text-myGreenD px-2 font-semibold">
-              Confirm Password
-            </legend>
-            <input
-              type="password"
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-              className="bg-transparent outline-none w-full"
-            />
-          </fieldset>
-          {/* INPUT CONFIRM PASSWORD */}
+          <InputPassword
+            label="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
 
-          {/* INDICATOR */}
-          <p className="text-center text-lg text-red-500">
-            {invalid && "Confirm Password Do Not Match"}
-          </p>
-          {/* INDICATOR */}
+          {/* INVALID */}
+          <p className="text-center text-lg text-red-500">{invalid && invalidLabel}</p>
 
           {/* REGISTER BUTTON */}
           <button
             type="submit"
-            className="h-11 w-full rounded-md bg-myGreenD/70 font-bold hover:bg-myGreenD/80"
+            disabled={loading}
+            className={`h-11 w-full flex items-center justify-center gap-1 rounded-md font-bold ${
+              loading ? "bg-myGreenD/60 cursor-wait" : "bg-myGreenD/70 hover:bg-myGreenD/80"
+            }`}
           >
-            REGISTER
+            <span>REGISTER</span>
+            {loading && (
+              <Image
+                src={require("@/assets/loading/Loading-Black.svg")}
+                alt="Loading"
+                width={20}
+                height={0}
+                quality={50}
+                loading="eager"
+                className="stroke-black"
+              />
+            )}
           </button>
-          {/* REGISTER BUTTON */}
         </form>
 
         {/* COPYRIGHT */}
         <p className="text-center">© 2023 FinCash. All rights reserved.</p>
-        {/* END COPYRIGHT */}
       </div>
     </main>
   );
