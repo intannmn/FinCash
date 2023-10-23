a87803e533784f1d4585add291ef15b878f34827
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-23 15:17:19 +0700

    update - Navbar | Header | Landing Page

--- src/components/landingpage/Header.jsx
+++ src/components/landingpage/Header.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import FinCash from "@/assets/logo/Logo.svg";
 
 export default function Header() {
   return (
     <header>
       <nav className="flex h-14 w-full items-center justify-between px-5">
         <div className="w-20">
-          <Image src={require("@/assets/logo/Logo.svg")} alt="FinCash" height={32.19} width={23} />
+          <Image src={FinCash} alt="FinCash" height={35} width={25} />
         </div>
         <ul className="flex items-center justify-start gap-8">
           <li className="font-semibold hover:text-myGreenD">
