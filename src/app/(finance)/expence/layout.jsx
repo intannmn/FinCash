35f7515a7c695965ba46d27038748d44705954c4
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-24 20:47:23 +0700

    feat - Expense

---
+++ src/app/(finance)/expence/layout.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "FinCash | Expence",
+};
+
+export default function ExpenceLayout({ children }) {
+  return children;
+}
---
+++ src/app/(finance)/expence/page.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/finance/expense/Header";
+import Main from "@/components/finance/expense/Main";
+import Footer from "@/components/finance/expense/Footer";
+
+export default function Expense() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
