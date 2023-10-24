9cc996d84fa602137410e2475d2e227ba777af64
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-24 20:50:31 +0700

    update - Dashboard

---
+++ src/app/(finance)/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/finance/dashboard/Header";
+import Main from "@/components/finance/dashboard/Main";
+import Footer from "@/components/finance/dashboard/Footer";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
