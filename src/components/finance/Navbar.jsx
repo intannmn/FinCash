2496cb64c4e32dcf0ead212ea03e178c666ab547
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-25 02:13:37 +0700

    update - Navigation Sidebar

--- src/components/finance/Navbar.jsx
+++ src/components/finance/Navbar.jsx
@@ -1,36 +1,35 @@
+import FinCash from "@/assets/logo/FinCash.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { FaMoneyBillTransfer } from "react-icons/fa6";
+import { GiPayMoney, GiReceiveMoney } from "react-icons/gi";
+import { RiDashboardFill } from "react-icons/ri";
 import Profile from "./Profile";
-import FinCash from "@/assets/logo/FinCash.svg";
 
 export default function Navbar() {
   return (
-    <nav className="fixed left-0 z-50 h-screen w-72">
-      <div className="absolute -z-10 h-full w-full bg-gradient-to-b from-myGreenA/80 to-white/50 bg-cover bg-fixed bg-no-repeat" />
-      <div className="absolute -z-[11] h-full w-full bg-dots-background bg-cover bg-fixed bg-no-repeat" />
-      <div className="flex h-full w-full flex-col bg-black/20">
-        <section>
-          <Image src={FinCash} alt="FinCash" width={250} className="mx-auto my-14" />
-        </section>
-        <section className="h-full w-full p-5">
-          <h1 className="text-llg font-bold">MANU</h1>
-          <div className="ml-5 text-lg font-semibold">
-            <Link href={"/dashboard"} className="block hover:text-myGreenC">
-              + Dashboard
-            </Link>
-            <Link href={"/expence"} className="block hover:text-myGreenC">
-              + Expence
-            </Link>
-            <Link href={"/income"} className="block hover:text-myGreenC">
-              + Income
-            </Link>
-            <Link href={"/report"} className="block hover:text-myGreenC">
-              + Report
-            </Link>
-          </div>
-        </section>
-        <Profile />
-      </div>
+    <nav className="fixed left-0 z-50 flex h-screen w-72 flex-col border-r-2 border-black bg-white">
+      <section className="border-b-2 border-black">
+        <Image src={FinCash} alt="FinCash" width={250} className="mx-auto my-14" />
+      </section>
+      <section className="h-full w-full p-5">
+        <h1 className="text-2xl font-bold">MANU</h1>
+        <div className="mt-4 space-y-4">
+          <Link href="/dashboard" className="flex items-center gap-2 hover:text-myGreenC">
+            <RiDashboardFill size={20} /> Dashboard
+          </Link>
+          <Link href="/expence" className="flex items-center gap-2 hover:text-myGreenC">
+            <GiPayMoney size={20} /> Expense
+          </Link>
+          <Link href="/income" className="flex items-center gap-2 hover:text-myGreenC">
+            <GiReceiveMoney size={20} /> Income
+          </Link>
+          <Link href="/report" className="flex items-center gap-2 hover:text-myGreenC">
+            <FaMoneyBillTransfer size={20} /> Report
+          </Link>
+        </div>
+      </section>
+      <Profile />
     </nav>
   );
 }
--- src/components/finance/Profile.jsx
+++ src/components/finance/Profile.jsx
@@ -2,16 +2,26 @@
 
 import { useSession, signOut } from "next-auth/react";
 import Image from "next/image";
+import { RiShutDownLine } from "react-icons/ri";
 
 export default function Profile() {
   const session = useSession();
+  const user = session.data?.user;
+
   return (
-    <section className="flex h-24 w-full items-center justify-center">
-      <div className="flex gap-2">
-        <Image src={session.data?.user.image} alt="Photo Profile" height={50} width={50} className="rounded-full" />
-        <h2>{session.data?.user.name}</h2>
+    <section className="flex h-24 w-full items-center justify-between border-t-2 border-black px-2">
+      <Image src={user?.image} alt="Photo Profile" height={50} width={50} className="rounded-full" />
+      <div className="flex flex-col">
+        <h2 className="w-[180px] truncate text-lg font-semibold" title={user?.name}>
+          {user?.name}
+        </h2>
+        <p className="w-[180px] truncate text-sm text-gray-500" title={user?.email}>
+          {user?.email}
+        </p>
       </div>
-      <button onClick={() => signOut()}>Sign Out</button>
+      <button onClick={() => signOut()} className="text-red-500">
+        <RiShutDownLine size={20} />
+      </button>
     </section>
   );
 }
