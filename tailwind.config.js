1120256e2f58180f6b24e3af243958438c547a21
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-25 07:41:06 +0700

    dependencies - Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
         "dots-background": "url('/assets/background/dots-background.svg')",
       },
       colors: {
-        myPurple: "#6152ff",
-        myPurpleSec: "#4e3eff",
-        myDarkPurple: "#2f3349",
         myGreenA: "#b6e6a2",
         myGreenB: "#5eda2a",
         myGreenC: "#dedf38",
