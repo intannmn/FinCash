196ae45aa97b22dccad1330e6b91e943fbc392a3
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-26 20:15:15 +0700

    update - Middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,2 +1,2 @@
 export { default } from "next-auth/middleware";
-export const config = { matcher: ["/dashboard", "/expense", "/income", "/report"] };
+export const config = { matcher: ["/dashboard", "/expense", "/income", "/report", "/account"] };
