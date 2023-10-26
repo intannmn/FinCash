7c8e133e2d50a6c76643485a8e9983163c181176
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-26 18:17:28 +0700

    update - Expense | Finance

--- src/app/(finance)/expense/page.jsx
+++ src/app/(finance)/expense/page.jsx
@@ -1,13 +1,5 @@
-import Header from "@/components/finance/expense/Header";
 import Main from "@/components/finance/expense/Main";
-import Footer from "@/components/finance/expense/Footer";
 
 export default function Expense() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+  return <Main />;
 }
--- src/components/finance/expense/Main.jsx
+++ src/components/finance/expense/Main.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import SubmitButton from "@/components/buttons/SubmitButton";
 import InputDate from "@/components/inputs/InputDate";
 import InputNumber from "@/components/inputs/InputNumber";
-import TextArea from "@/components/inputs/TextArea";
 import Select from "@/components/inputs/Select";
-import Image from "next/image";
+import TextArea from "@/components/inputs/TextArea";
+import { useState } from "react";
 
 export default function Main() {
   const [date, setDate] = useState("");
@@ -15,7 +15,7 @@ export default function Main() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <main className="px-5">
+    <main>
       <form className="space-y-3">
         <InputDate label="Date" value={date} onChange={(e) => setDate(e.target.value)} />
         <InputNumber label="Rp" value={rp} onChange={(e) => setRp(e.target.value)} />
@@ -26,16 +26,7 @@ export default function Main() {
           <option value="Other">Other</option>
         </Select>
         <TextArea label="Note" value={note} onChange={(e) => setNote(e.target.value)} />
-        <button
-          type="submit"
-          disabled={loading}
-          className={`flex h-10 w-24 items-center justify-center gap-1 rounded-md font-bold text-white ${
-            loading ? "cursor-wait bg-myGreenD/60" : "bg-myGreenD/70 hover:bg-myGreenD/80"
-          }`}
-        >
-          <span>Submit</span>
-          {loading && <Image src={require("@/assets/loading/Loading-Black.svg")} alt="Loading" width={20} height={0} quality={50} loading="eager" />}
-        </button>
+        <SubmitButton label={"Submit"} font={"semibold"} size={"md"} loading={loading} />
       </form>
     </main>
   );
