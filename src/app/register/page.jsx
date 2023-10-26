8945d4a82d082572daadfd0104c2e9ab96649d7a
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-26 18:44:56 +0700

    update - Register

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -4,7 +4,32 @@ import SubmitButton from "@/components/buttons/SubmitButton";
 import InputPassword from "@/components/inputs/InputPassword";
 import InputText from "@/components/inputs/InputText";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+
+async function createAccount(account) {
+  try {
+    const response = await fetch("https://653a4cfee3b530c8d9e975ac.mockapi.io/account", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(account),
+    });
+    return handleResponse(response);
+  } catch (error) {
+    console.error("An error occurred while sending data:", error);
+    throw error;
+  }
+}
+
+function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Request failed with status: " + response.status);
+  }
+}
 
 export default function Register() {
   const [username, setUsername] = useState("");
@@ -13,33 +38,41 @@ export default function Register() {
   const [invalid, setInvalid] = useState(false);
   const [invalidLabel, setInvalidLabel] = useState("");
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setTimeout(() => {
-      if (username && password && confirmPassword) {
-        if (password === confirmPassword) {
+    if (username && password && confirmPassword) {
+      if (password === confirmPassword) {
+        const account = {
+          username,
+          password,
+        };
+        try {
+          await createAccount(account);
+          router.push("/login");
+        } catch (error) {
           setLoading(false);
-          alert(`Username: ${username}\nPassword: ${password}`);
-        } else {
-          setLoading(false);
-          setInvalidLabel("Confirm Password Do Not Match");
-          setInvalid(true);
+          console.error("An error occurred while creating an account:", error);
         }
       } else {
         setLoading(false);
-        setInvalidLabel("Can Not Be Empty");
+        setInvalidLabel("Confirm Password Does Not Match");
         setInvalid(true);
       }
-    }, 2000);
+    } else {
+      setLoading(false);
+      setInvalidLabel("Fields Cannot Be Empty");
+      setInvalid(true);
+    }
   };
 
   return (
     <main className="flex h-screen w-screen items-center justify-center bg-gradient-to-b from-myGreenA/80 to-white/50">
       <div className="fixed -z-10 h-full w-full bg-dots-background bg-cover bg-fixed bg-no-repeat" />
       <div className="w-[400px] space-y-5 rounded-lg border-2 border-black bg-myGreenA/50 p-5">
-        <Image src={require("@/assets/logo/FinCash.svg")} alt="FinCash" quality={50} className="mx-auto" />
+        <Image src={require("@/assets/logo/FinCash.svg")} alt="FinCash" quality={50} className="mx-auto" priority />
         <form onSubmit={handleSubmit} className="mx-auto w-full space-y-5">
           <InputText label="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
           <InputPassword label="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
