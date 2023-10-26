f552d90e2473ef1a8809eeea38093e078155e856
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-26 16:05:29 +0700

    update - Finance Layout

--- src/app/(finance)/layout.jsx
+++ src/app/(finance)/layout.jsx
@@ -1,10 +1,14 @@
+import Header from "@/components/finance/Header";
 import Navbar from "@/components/finance/Navbar";
 
 export default function FinanceLayout({ children }) {
   return (
     <>
+      <div className="fixed left-0 top-0 -z-10 h-screen w-screen bg-gradient-to-b from-myGreenA/80 to-white/50 bg-cover bg-fixed bg-no-repeat" />
+      <div className="fixed left-0 top-0 -z-[11] h-screen w-screen bg-dots-background bg-cover bg-fixed bg-no-repeat" />
+      <Header />
       <Navbar />
-      <div className="ml-72">{children}</div>
+      <div className="ml-72 mt-28">{children}</div>
     </>
   );
 }
