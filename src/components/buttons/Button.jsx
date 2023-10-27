69b20db9ada84a4179b293971e4c9339c85d3584
Intan Meilanie Nugraha <intanmeilanienugraha@gmail.com>
2023-10-27 20:17:38 +0700

    comp - Button

---
+++ src/components/buttons/Button.jsx
@@ -0,0 +1,15 @@
+export default function Button({ label, font, size, onClick }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`flex items-center justify-center gap-1 rounded-md bg-A3 hover:bg-A4 ${font === "semibold" && "font-semibold"} ${
+        font === "bold" && "font-bold"
+      } ${size === "sm" && "rounded-sm px-3 py-1"} ${size === "sm-full" && "w-full rounded-sm py-1"} ${size === "md" && "rounded-md px-6 py-2"} ${
+        size === "md-full" && "w-full rounded-md py-2"
+      }  ${size === "lg" && "rounded-md px-12 py-3"} ${size === "lg-full" && "w-full rounded-md py-3"}`}
+    >
+      <span>{label}</span>
+    </button>
+  );
+}
